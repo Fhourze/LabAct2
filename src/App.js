ce162b22254444598646855a5c353000449c45e7
Fhourze <124254334+Fhourze@users.noreply.github.com>
2024-02-19 14:49:53 +0800

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ function App() {
           brand="Megabox"
           productName="Megabox Dish Drainer MG323 – White"
           price="P599"
+          quantity="1"
           imageUrl="https://www.acehardware.ph/cdn/shop/products/13a-jpg_60257_1024x1024.jpg?v=1630204240"
         />
         <Product
-          brand="Example Brand"
+          brand="Orocan"
           productName="Orocan Kitchen Queen 8722M Dish Drainer (White)"
           price="P1,647"
+          quantity="0"
           imageUrl="https://www.acehardware.ph/cdn/shop/products/Orocan8722M_1024x1024.png?v=1631512754"
         />
       </div>
